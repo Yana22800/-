e97423e8b2ba474a122ae5b9b1cb1b3e7a4c0ccd
Yana22800 <yanusik1043@gmail.com>
2021-12-23 19:24:06 +0300

    Initial commit

---
+++ Инь/MeetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Инь.Models;
+
+namespace Инь
+{
+    public class MeetManager
+    {
+        List<Meet> meets = new List<Meet>();
+        public void AddNewMeet(string name, DateTime meetTime)
+        {
+            Meet newMeet = new Meet() { Name = name, CreateTime = DateTime.Now };
+            meets.Add(newMeet);
+        }
+    }
+}
