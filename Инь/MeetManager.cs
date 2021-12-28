087f3791c4d0aa8ff8c213c5ecdd1a3341afa3a3
Yana22800 <yanusik1043@gmail.com>
2021-12-28 19:14:07 +0300

    ghjghjgjgjffjfgj
    c xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxs

--- Инь/MeetManager.cs
+++ Инь/MeetManager.cs
@@ -7,7 +7,12 @@ namespace Инь
 {
     public class MeetManager
     {
-        List<Meet> meets = new List<Meet>();
+        private List<Meet> meets = new List<Meet>();
+
+        public List<Meet> GetAll()
+        {
+            return meets;
+        }
         public void AddNewMeet(string name, DateTime meetTime)
         {
             Meet newMeet = new Meet() { Name = name, CreateTime = DateTime.Now };
--- Инь/Program.cs
+++ Инь/Program.cs
@@ -29,6 +29,13 @@ namespace Инь
             DateTime user_meetTime = new DateTime(user_meetTimeYear, user_meetTimeMonth, user_meetTimeDay, user_meetTimeHour, user_meetTimeMinute, 0);
 
             meetManager.AddNewMeet(user_nameMeet, user_meetTime);
+
+            foreach (var item in meetManager.GetAll())
+            {
+                Console.WriteLine($"Имя встречи:{item.Name}");
+                Console.WriteLine($"Время встречи:{item.MeetTime}\r\n");
+            }
+
             Console.ReadKey();
         }
     }
